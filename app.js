390fef9ff38a88691c91244fffec34a5ed824790
wulanfrom <biasindi.wulanfrom@gmail.com>
2020-10-30 05:17:04 +0900

    added navbar and footer and started homepage

---
+++ app.js
@@ -0,0 +1,33 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger-menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const shadowBackground = document.querySelector('.shadow-background');
+
+    //Toggle Navbar
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 8}s`
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+
+        //Add shadow background
+        shadowBackground.classList.toggle('shadow');
+    })
+}
+
+const app = () => {
+    navSlide();
+}
+
+app()
