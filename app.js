d5913893e254bc989fa4e8a49059233610986af1
wulanfrom <biasindi.wulanfrom@gmail.com>
2020-12-09 20:15:33 +0900

    updated call for papers and added scroll button

--- app.js
+++ app.js
@@ -1,33 +1,74 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger-menu');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    const shadowBackground = document.querySelector('.shadow-background');
+window.onload = function() {
+    const navSlide = () => {
+        const burger = document.querySelector('.burger-menu');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+        const shadowBackground = document.querySelector('.shadow-background');
+    
+        //Toggle Navbar
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+    
+            //Animate Links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation){
+                    link.style.animation = '';
+                }
+                else{
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 8}s`
+                }
+            });
+    
+            //Burger Animation
+            burger.classList.toggle('toggle');
+    
+            //Add shadow background
+            shadowBackground.classList.toggle('shadow');
+        })
+    }
+    
+    const goToSocialMedia = () => {
+        document.getElementById("twitter").onclick = function() {
+            location.href='https://twitter.com/asian_chi_conf';
+        };
 
-    //Toggle Navbar
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+        document.getElementById("facebook").onclick = function() {
+            location.href='https://www.facebook.com/Asian-CHI-2021-Symposium-105802457964677';
+        };
+    }
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = '';
+    //Get the button:
+    const goToTopButton = () => {
+        const mybutton = document.getElementById("goToTop");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() { scrollFunction() };
+    
+        const scrollFunction = () => {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
             }
-            else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 8}s`
-            }
-        });
+        }
+    
+        // When the user clicks on the button, scroll to the top of the document
+        mybutton.addEventListener('click', () => {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        })
+        // mybutton.onclick(function() {
 
-        //Burger Animation
-        burger.classList.toggle('toggle');
-
-        //Add shadow background
-        shadowBackground.classList.toggle('shadow');
-    })
+        // })
+    }
+    
+    const app = () => {
+        navSlide();
+        goToSocialMedia();
+        // scrollFunction();
+        // topFunction();
+        goToTopButton();
+    }
+    
+    app()
 }
-
-const app = () => {
-    navSlide();
-}
-
-app()
